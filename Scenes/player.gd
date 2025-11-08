74086625201e0f4358caebf63ff4257b008b1f88
anvvvi <andreighelase69@proton.me>
2025-11-08 13:19:01 +0200

    Made the player have collision

--- Scenes/player.gd
+++ Scenes/player.gd
@@ -1,10 +1,8 @@
-extends Sprite2D
-
-var pos: Vector2 = Vector2(0,0)
+extends CharacterBody2D
 
 var direction: Vector2
 
-var speed = 10
+var speed = 300
 
 # Called when the node enters the scene tree for the first time.
 func _ready() -> void:
@@ -13,6 +11,6 @@ func _ready() -> void:
 
 # Called every frame. 'delta' is the elapsed time since the previous frame.
 func _process(delta: float) -> void:
-	position = pos
 	direction = Input.get_vector("Move_Left","Move_Right","Move_Up","Move_Down")
-	pos += direction * speed
+	velocity = speed * direction
+	move_and_slide()
