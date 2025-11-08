620667e57e40fbfd4eb1cb0487be9e42e1ccfd89
anvvvi <andreighelase69@proton.me>
2025-11-08 13:21:26 +0200

    Removed the test icon.svg

--- Scenes/player.gd
+++ Scenes/player.gd
@@ -4,12 +4,9 @@ var direction: Vector2
 
 var speed = 300
 
-# Called when the node enters the scene tree for the first time.
 func _ready() -> void:
-	pass # Replace with function body.
+	pass
 
-
-# Called every frame. 'delta' is the elapsed time since the previous frame.
 func _process(delta: float) -> void:
 	direction = Input.get_vector("Move_Left","Move_Right","Move_Up","Move_Down")
 	velocity = speed * direction
