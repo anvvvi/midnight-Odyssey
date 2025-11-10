c4e37e3d8d1a04b37511c5c0ef5bb91c979e3fb9
anvvvi <andreighelase69@proton.me>
2025-11-10 16:08:24 +0200

    Made a basic health ui and cleaned the folders up slighty

--- Scenes/Player/player.gd
+++ Scenes/Player/player.gd
@@ -2,7 +2,9 @@ extends CharacterBody2D
 
 var direction: Vector2
 
-var speed = 300
+var speed = 500
+
+var health = 5
 
 func _ready() -> void:
 	pass
@@ -10,4 +12,8 @@ func _ready() -> void:
 func _process(delta: float) -> void:
 	direction = Input.get_vector("Move_Left","Move_Right","Move_Up","Move_Down")
 	velocity = speed * direction
-	move_and_slide()
+	if (move_and_slide()):
+		print("Collided")
+		health -= 1
+	else:
+		print("NotColliding")  
